1a8c558fe76c9dee90a89fd8bad596a000954b84
Hodor Dell <geoffrey.aulombard@gmail.com>
2022-09-17 12:22:40 +0200

    first commit

---
+++ src/main/java/com/hodor/housekepperapi/HouseKepperApiApplication.java
@@ -0,0 +1,13 @@
+package com.hodor.housekepperapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseKepperApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HouseKepperApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hodor/housekepperapi/HouseKepperApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hodor.housekepperapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouseKepperApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
